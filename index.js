f7295e901c1d20290672ffd0c5997b5f01975a09
Aaryan Pashine <pashineaaryan@gmail.com>
2026-05-29 22:31:19 -0400

    ok

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/api', async (req, res) => {
-  const response = await fetch('http://localhost:11434/api/ps');
+  let response = await fetch('http://localhost:11434/api/ps');
 	console.log("GOT", response)
-	let res = response.json()
-	console.log(res)
+	response = response.json()
+	console.log(response)
 });
 
 app.listen(PORT, () => {
